428fd02c2a621cab7e0e3da4d65b8d9a092b37d6
srinivas-kct <41998649+srinivas-kct@users.noreply.github.com>
2018-08-01 18:53:34 +0530

    Add files via upload

---
+++ prefix.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int n,i,len=0,j;
+    scanf("%d",&n);
+    char a[n][100000];
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",a[i]);
+    }
+    len=strlen(a[i]);
+    for(i=0;i<n;i++)
+    {
+        if(len>strlen(a[i]));
+            len=strlen(a[i]);
+
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<len;j++)
+        {
+            if(a[i][j]==a[i+1][j])
+            {
+
+            }
+            else
+                break;
+        }
+    }
+    for(i=0;i<j;i++)
+        printf("%c",a[0][i]);
+}
